1f748d3423c04026460a7b9909fd1c0d12afe0b5
Ainhize <ainhize.uribesalgo@alumni.mondragon.edu>
2022-11-29 08:35:29 +0100

    maven + spring project created

---
+++ src/main/java/gruner/huger/grunerhugel/GrunerhugelApplication.java
@@ -0,0 +1,13 @@
+package gruner.huger.grunerhugel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrunerhugelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrunerhugelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gruner/huger/grunerhugel/GrunerhugelApplicationTests.java
@@ -0,0 +1,13 @@
+package gruner.huger.grunerhugel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrunerhugelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
