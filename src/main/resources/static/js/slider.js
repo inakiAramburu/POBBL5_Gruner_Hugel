74bc02d62b188a80f6a6f5d7e68635bd81ef7cda
Ainhize <ainhize.uribesalgo@alumni.mondragon.edu>
2023-01-07 11:17:21 +0100

    slider añadido

---
+++ src/main/resources/static/js/slider.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+
+    var sliderTrigger = document.getElementById("slider-trigger");
+    var slider = document.getElementById("slider-content");
+
+    sliderTrigger.addEventListener("click", function() {
+        if(slider.classList.contains("active")){  
+            slider.classList.remove("active"); 
+        }else{  
+            slider.classList.add("active"); 
+        }
+    });
+}
