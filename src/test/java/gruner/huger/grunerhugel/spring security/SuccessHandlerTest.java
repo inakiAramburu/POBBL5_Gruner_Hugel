765e0a9383170451bd70dafb696ad0c9999f7e75
inaki.aramburur <inaki.aramburur@alumni.mondragon.edu>
2023-01-19 09:49:05 +0100

    test lins

--- src/test/java/gruner/huger/grunerhugel/spring security/SuccessHandlerTest.java
+++ src/test/java/gruner/huger/grunerhugel/spring security/SuccessHandlerTest.java
@@ -1,33 +1,61 @@
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Timeout;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import gruner.huger.grunerhugel.simulation.SimulationProcesses;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import gruner.huger.grunerhugel.authentication.CustomSuccesHandler;
+import gruner.huger.grunerhugel.domain.repository.FarmRepository;
+import gruner.huger.grunerhugel.domain.repository.UserRepository;
+import gruner.huger.grunerhugel.model.Farm;
+import gruner.huger.grunerhugel.model.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @SpringBootTest
 class SuccessHandlerTest {
-	SimulationProcesses sim;
+    HttpServletRequest request ;
+    HttpServletResponse response;
+    Authentication authentication;
+    @Autowired
+    UserRepository userRepository;
+    FarmRepository farmRepository;
+    
 
-	// //@Before
-	// void contextLoads() {
-		
-	// 	System.out.println("Test");
 
-	// }
+@Test
+public void determineTargetURLTest() {
+    Authentication authAdmin = mock(Authentication.class);
+    Authentication authUser = mock(Authentication.class);
+    Authentication authInvestor = mock(Authentication.class);
+    
+    //Admin
+    when(authAdmin.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ADMIN"))).thenReturn(true);
+    String expectedAdminURL = "/admin";
 
-	// @Test
-	// @Timeout(value= 36000)
-	// void test() throws InterruptedException {
-	// 	sim = new SimulationProcesses(0, null, null);
-	// 	sim.run();
-	// }
+    CustomSuccesHandler sa=new CustomSuccesHandler();
+    String actualAdminURL = sa.determineTargetUrl(authAdmin);
+    assertEquals(expectedAdminURL, actualAdminURL);
 
-	// @Test
-	// @Timeout(value = 16000)
-	// void test2() throws InterruptedException {
-	// 	sim = new SimulationProcesses(0, null, null);
-	// 	sim.setAccelerator(2);
-	// 	sim.run();
-	// }
-
+      /*  //User 
+        when(authUser.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("USER"))).thenReturn(true);
+        when(userRepository.findByUsername(authUser.getName())).thenReturn(user);
+        when(farmRepository.findByUser(user)).thenReturn(farm);
+        String expectedUserURL = "/simulation";
+        String actualUserURL = determineTargetUrl(authUser);
+        assertEquals(expectedUserURL, actualUserURL);
+        */
+}
+    
 }
