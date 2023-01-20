e3f98e8979c362f9d806273da416adf12c19e4ce
inaki.aramburur <inaki.aramburur@alumni.mondragon.edu>
2023-01-20 10:56:21 +0100

    prueba 2

---
+++ src/test/java/gruner/huger/grunerhugel/UserTest.java
@@ -0,0 +1,48 @@
+package gruner.huger.grunerhugel;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import gruner.huger.grunerhugel.model.Role;
+import gruner.huger.grunerhugel.model.User;
+
+
+class UserTest {
+    @Test
+	void testUsers() {
+        Role role = new Role("ROLE_USER");
+        User user = new User("nametest","surnametest","sad@gmail.com","user","pass",role);
+        
+        assertEquals("nametest", user.getName());
+        assertEquals("surnametest", user.getSurname());
+        assertEquals("sad@gmail.com",user.getEmail());
+        assertEquals("user",user.getUsername());
+        assertEquals("pass",user.getPassword());
+        assertEquals(role,user.getRole());
+	}
+    @Test
+	void testGetSet() {
+
+        User user = new User();
+        Role role = new Role("ROLE_USER");
+        user.setName("nametest");
+        user.setSurname("surnametest");
+        user.setEmail("sad@gmail.com");
+        user.setUsername("user");
+        user.setPassword("pass");
+        user.setRole(role);
+
+        assertEquals("nametest", user.getName());
+        assertEquals("surnametest", user.getSurname());
+        assertEquals("sad@gmail.com",user.getEmail());
+        assertEquals("user",user.getUsername());
+        assertEquals("pass",user.getPassword());
+        assertEquals(role,user.getRole());
+	}
+
+
+}
