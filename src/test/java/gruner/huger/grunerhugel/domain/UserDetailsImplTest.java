5f2d6ffd27a43e07817b3e83e254127f7a4fb5b8
Ainhize <ainhize.uribesalgo@alumni.mondragon.edu>
2023-01-23 09:04:11 +0100

    validator passed and made some changes

--- src/test/java/gruner/huger/grunerhugel/domain/UserDetailsImplTest.java
+++ src/test/java/gruner/huger/grunerhugel/domain/UserDetailsImplTest.java
@@ -1,7 +1,5 @@
 package gruner.huger.grunerhugel.domain;
 
-
-
 import gruner.huger.grunerhugel.domain.user.UserDetailServiceImpl;
 import gruner.huger.grunerhugel.domain.user.UserDetailsImpl;
 import gruner.huger.grunerhugel.model.Role;
@@ -16,26 +14,22 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 public class UserDetailsImplTest {
-    
+
     @Test
-    public void coverage(){
-    User user =new User();
-    Role role =new Role("ADMIN");
-    user.setRole(role);
-    UserDetailsImpl userDetailsImpl =new UserDetailsImpl(user);
-    Collection<? extends GrantedAuthority> authorities = new ArrayList<>();
-    
-    
-    userDetailsImpl.getAuthorities();
-    userDetailsImpl.getPassword();
-    userDetailsImpl.getUsername();
-    userDetailsImpl.isAccountNonExpired();
-    userDetailsImpl.isAccountNonLocked();
-    userDetailsImpl.isCredentialsNonExpired();
-    userDetailsImpl.isEnabled();
-    
-    
-    
+    public void coverage() {
+        User user = new User();
+        Role role = new Role("ADMIN");
+        user.setRole(role);
+        UserDetailsImpl userDetailsImpl = new UserDetailsImpl(user);
+        Collection<? extends GrantedAuthority> authorities = new ArrayList<>();
+
+        userDetailsImpl.getAuthorities();
+        userDetailsImpl.getPassword();
+        userDetailsImpl.getUsername();
+        userDetailsImpl.isAccountNonExpired();
+        userDetailsImpl.isAccountNonLocked();
+        userDetailsImpl.isCredentialsNonExpired();
+        userDetailsImpl.isEnabled();
 
     }
 }
